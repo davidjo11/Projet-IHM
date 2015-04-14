4abc34696cfd4fa1422946e909d04658be283691
lan21 <allan.rakotoarivony@gmail.com>
2015-04-14 15:40:55 +0200

    emploi par défaut

--- example03/script.js
+++ example03/script.js
@@ -95,7 +95,7 @@ save = function () {
 // function shows subject occurring in timetable
 report = function (subject) {
 		// define day and time labels
-	var day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+	var day = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'],
 		time = ['08:00', '09:00', '10:00', '11:00', '12:00',
 		        '13:00', '14:00', '15:00', '16:00', '17:00',
 		        '18:00', '19:00', '20:00'],
